6b322fc654f411046c21067a7266dad4ff0bacc7
Macky Beltran <mackybeltran1@gmail.com>
2017-02-09 23:17:16 +0000

    add loaded die exrercise

---
+++ die.js
@@ -0,0 +1,20 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  /* your code here */
+  var n = 0
+  // var currentNumber = list[n]
+
+
+  return function() {
+    /* your code here */
+    var currentNumber = list[n];
+    n += 1
+    return currentNumber;
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
